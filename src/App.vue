8988a222bd9f6541d00505caef21f02128e67034
rbelov <rbelov@synergy.ru>
2022-07-22 21:14:55 +0300

    build project and layout

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <SMain />
+  </div>
+</template>
+
+<script>
+import SMain from "@/components/sections/s_main";
+export default {
+  name: "App",
+  components: {
+    SMain,
+  },
+};
+</script>
---
+++ src/components/atoms/a_button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    @click="btnClick"
+    class="w-full mb-2 md:w-auto md:mr-4 px-4 py-2 rounded-md text-white"
+    v-html="label"
+  ></button>
+</template>
+
+<script>
+export default {
+  name: "AButton",
+  props: {
+    label: String,
+  },
+  methods: {
+    btnClick() {
+      this.$emit("btnClick");
+    },
+  },
+};
+</script>
